23b02c8d6e02b4368a04e70f339154d895e91224
unknown <luca.canizzo@gmail.com>
2017-11-02 10:27:42 -0400

    Products model set up

---
+++ models/products.js
@@ -0,0 +1,31 @@
+const orm = require('../config/orm.js');
+
+const product = {
+    all: (cb)=>{
+        orm.all("products", (res)=>{
+            cb(res);
+        });
+    },
+    allByOne: (cond, val, cb)=>{
+        orm.allBy("products", cond, val, (res)=>{
+            cb(res);
+        });
+    },
+    create: (cols, vals, cb)=>{
+        orm.create("products", cols, vals, (res)=>{
+            cb(res);
+        });
+    },
+    update: (cond, val, cb)=>{
+        orm.update("products", cond, val, (res)=>{
+            cb(res);
+        });
+    },
+    deleteAllByOne: (cond, val, cb)=>{
+        orm.deleteAllByOne("products", cond, val, (res)=>{
+            cb(res);
+        });
+    }
+};
+
+module.exports = product;
